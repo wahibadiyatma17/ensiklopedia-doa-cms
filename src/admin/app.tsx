65e44b867230d5862e2ec6f86a29031cd4e512fa
wahibadiyatma17 <wahib.adiyatma17@gmail.com>
2025-11-18 09:54:09 +0700

    chore: update text

--- src/admin/app.tsx
+++ src/admin/app.tsx
@@ -3,6 +3,12 @@ import type { StrapiApp } from '@strapi/strapi/admin';
 export default {
   config: {
     locales: [],
+    translations: {
+      en: {
+        'Auth.form.welcome.title': 'Welcome to Ensiklopedia Doa CMS',
+        'Auth.form.welcome.subtitle': 'Log in to your account',
+      },
+    },
   },
   bootstrap(app: StrapiApp) {
     console.log('Admin customization loaded');
